2e4ea98295b7fb029cd4ac39ec38c531931b7a90
Rafal Garbacik <rafalgbc@gmail.com>
2023-06-10 16:02:23 +0200

    Initial commit

--- src/psmproject/dashboardapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from django.urls import include
-from dashboardapp import views
-
-urlpatterns = [
-    path("", views.DashboardView.as_view(), name="dashboard"),
-]
--- src/psmproject/dashboardapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views.generic.base import TemplateView
-
-
-# Create your views here.
-
-
-class DashboardView(TemplateView):
-    template_name = "dashboardapp/dashboard.html"
