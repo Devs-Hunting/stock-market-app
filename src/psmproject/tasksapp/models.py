affb0a8162f84499fbca45b109253058ba3d1c65
Bartosz Wisniewski <bartekwisnia@op.pl>
2023-06-13 10:21:18 +0200

    Task and TaskAttachment models

--- src/psmproject/tasksapp/models.py
+++ src/psmproject/tasksapp/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+UserModel = get_user_model()
+
+
+class Task(models.Model):
+    class TaskStatus(models.IntegerChoices):
+        OPEN = 0, _("open")
+        CLOSED = 1, _("closed")
+        ON_GOING = 2, _("on-going")
+        HOLD = 3, _("hold")
+        COMPLETED = 4, _("completed")
+        CANCELLED = 5, _("cancelled")
+
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=30)
+    realization_time = models.DateField()
+    budget = models.FloatField()
+    client = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    status = models.IntegerField(
+        choices=TaskStatus.choices, default=TaskStatus.OPEN
+    )
+    # selected_offer = models.OneToOneField(Offer, related_name="in_task", null=True, on_delete=models.SET_NULL)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+class TaskAttachment(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name="attachments")
+    attachment = models.FileField(upload_to="attachments/")
