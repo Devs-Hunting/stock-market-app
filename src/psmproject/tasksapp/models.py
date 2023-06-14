16761db24f300856b37e7165316e573cf356bc2d
Bartosz Wisniewski <bartekwisnia@op.pl>
2023-06-14 11:13:27 +0200

    Added Doc Strings, Task model corrections, TaskAttachement validation methods

--- src/psmproject/tasksapp/models.py
+++ src/psmproject/tasksapp/models.py
@@ -1,23 +1,29 @@
+from django.conf import settings
+from django.core.exceptions import ValidationError
 from django.db import models
-from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
 
-UserModel = get_user_model()
+
+UserModel = settings.AUTH_USER_MODEL
 
 
 class Task(models.Model):
+    """
+    This model represents a Task. It includes information such as the title, description,
+    expected realization time, budget, client, task status, and the creation and update dates.
+    """
     class TaskStatus(models.IntegerChoices):
         OPEN = 0, _("open")
         CLOSED = 1, _("closed")
         ON_GOING = 2, _("on-going")
-        HOLD = 3, _("hold")
+        OBJECTIONS = 3, _("objections")
         COMPLETED = 4, _("completed")
         CANCELLED = 5, _("cancelled")
 
     title = models.CharField(max_length=120)
     description = models.TextField()
     realization_time = models.DateField()
-    budget = models.FloatField()
+    budget = models.DecimalField()
     client = models.ForeignKey(UserModel, on_delete=models.CASCADE)
     status = models.IntegerField(
         choices=TaskStatus.choices, default=TaskStatus.OPEN
@@ -28,6 +34,39 @@ class Task(models.Model):
 
 
 class TaskAttachment(models.Model):
+    """
+    This model represents a TaskAttachment. It includes information such as the related task,
+    the attached file, and the creation and update dates of the attachment.
+    """
+    MAX_ATTACHMENTS = 10
+
     task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name="attachments")
-    attachment = models.FileField(upload_to="attachments/")
+
+    @staticmethod
+    def get_upload_path(instance, filename):
+        """Generates the file path for the TaskAttachment."""
+        return f'attachments/tasks/{instance.task.id}/{filename}'
+
+    attachment = models.FileField(upload_to=get_upload_path)
+    created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    def clean(self):
+        """
+        Custom clean method that checks the number of attachments for the related task and
+        throws an error if the limit is exceeded.
+        """
+        existing_attachments = TaskAttachment.objects.filter(task=self.task).count()
+
+        if existing_attachments >= TaskAttachment.MAX_ATTACHMENTS:
+            raise ValidationError('You have reached the maximum number of attachments for this task.')
+
+    def save(self, *args, **kwargs):
+        """
+        Custom save method that checks for an existing attachment with the same name for
+        the related task and deletes it if found before saving the new one.
+        """
+        existing = TaskAttachment.objects.filter(task=self.task, attachment=self.attachment.name)
+        existing.delete()
+
+        super().save(*args, **kwargs)
