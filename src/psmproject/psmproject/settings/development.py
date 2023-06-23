1ee5587c77caf058ed74dba19f1e59f79a345352
ndestrieux <n.destrieux@gmail.com>
2023-06-23 17:01:25 +0200

    facebook connection removed as it will be implemented

--- src/psmproject/psmproject/settings/development.py
+++ src/psmproject/psmproject/settings/development.py
@@ -144,7 +144,6 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 SITE_ID = 1
 LOGIN_REDIRECT_URL = "profile"
 
-
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
         'SCOPE': [
@@ -155,26 +154,4 @@ SOCIALACCOUNT_PROVIDERS = {
             'access_type': 'online',
         }
     },
-    'facebook': {
-        'METHOD': 'oauth2',
-        'SDK_URL': '//connect.facebook.net/{locale}/sdk.js',
-        'SCOPE': ['email', 'public_profile'],
-        'AUTH_PARAMS': {'auth_type': 'reauthenticate'},
-        'INIT_PARAMS': {'cookie': True},
-        'FIELDS': [
-            'id',
-            'first_name',
-            'last_name',
-            'middle_name',
-            'name',
-            'name_format',
-            'picture',
-            'short_name'
-        ],
-        'EXCHANGE_TOKEN': True,
-        'LOCALE_FUNC': 'path.to.callable',
-        'VERIFIED_EMAIL': False,
-        'VERSION': 'v13.0',
-        'GRAPH_API_URL': 'https://graph.facebook.com/v13.0',
-    }
 }
