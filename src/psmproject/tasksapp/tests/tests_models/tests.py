b223936cc0d7828297c634bdb17fa74cdbb8c296
Adam <adamjawor@gmail.com>
2023-06-28 19:52:34 +0200

    [DEV-53] Test for  Task Attachment

--- src/psmproject/tasksapp/tests/tests_models/tests.py
+++ src/psmproject/tasksapp/tests/tests_models/tests.py
@@ -1,3 +1,7 @@
+import os
+import shutil
+
+from django.core.exceptions import ValidationError
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import Client, TestCase
 from tasksapp.models import Task, TaskAttachment
@@ -18,7 +22,7 @@ class TestTaskModel(TestCase):
             title="Test title",
             description="Test description",
             realization_time="2023-12-01",
-            budget=1200.12,
+            budget=1200.1200,
             client=cls.test_user,
         )
 
@@ -26,12 +30,18 @@ class TestTaskModel(TestCase):
     def tearDownClass(cls) -> None:
         return super().tearDownClass()
 
-    def test_should_return_max_lenght_for_title_field(self):
+    def test_should_return_max_length_for_title_field(self):
+        """
+        Test that check max length of title field.
+        """
         task = Task.objects.get(id=1)
         max_length = task._meta.get_field("title").max_length
         self.assertEqual(max_length, 120)
 
     def test_should_return_correct_number_for_budget_field(self):
+        """
+        Test check that budget field is decimal number.
+        """
         expected_number = "1200.12"
         task = Task.objects.get(id=1)
         actual_number = str(task.budget)
@@ -39,6 +49,9 @@ class TestTaskModel(TestCase):
         self.assertEqual(expected_number, actual_number)
 
     def test_should_return_default_status_field_as_status_of_task_is_open(self):
+        """
+        Test check that status field has his default value = 0.
+        """
         expected_status = 0
         task = Task.objects.get(id=1)
         actual_status = task.status
@@ -46,6 +59,9 @@ class TestTaskModel(TestCase):
         self.assertEqual(expected_status, actual_status)
 
     def test_should_return_correct_text_as_represenation_of_object(self):
+        """
+        Test check that the representation of object Task has correct text.
+        """
         expected_represenation = "<Task id=1, title=Test title>"
         task = Task.objects.get(id=1)
         actual_representation = repr(task)
@@ -53,6 +69,9 @@ class TestTaskModel(TestCase):
         self.assertEqual(expected_represenation, actual_representation)
 
     def test_should_return_correct_string_representation(self):
+        """
+        Test check that the string representation of instance of object Task has correct text.
+        """
         expected_string = "Task: Test title"
         task = Task.objects.get(id=1)
         actual_string = str(task)
@@ -81,32 +100,54 @@ class TestTaskAttachmentModel(TestCase):
             task=cls.test_task,
             attachment=SimpleUploadedFile("test_file.txt", b"content of test file"),
         )
-        cls.test_task_attachment2 = TaskAttachment(
-            task=cls.test_task,
-            attachment=SimpleUploadedFile("test_file.txt", b"content of test file"),
-        )
-        cls.test_task_attachment2.save()
 
     @classmethod
     def tearDownClass(cls) -> None:
-        return super().tearDownClass()
+        super().tearDownClass()
+        file_path = os.path.join("attachments/tasks/1/")
+        shutil.rmtree(file_path, ignore_errors=True)
 
     def test_should_raise_Validation_Error_when_max_attachments_number_is_exceeded(
         self,
     ):
-        pass
+        """
+        Test checks that when max number of attachments is exceeded is raised ValidationError.
+        """
+        for index in range(TaskAttachment.MAX_ATTACHMENTS):
+            TaskAttachment.objects.create(
+                task=self.test_task,
+                attachment=SimpleUploadedFile(
+                    f"test_file_{index}.txt", b"content of test file"
+                ),
+            )
+        self.task_attachment = TaskAttachment.objects.create(
+            task=self.test_task,
+            attachment=SimpleUploadedFile("test_file.txt", b"content of test file"),
+        )
+        with self.assertRaises(ValidationError):
+            self.task_attachment.clean()
 
     def test_should_check_and_delete_for_an_existing_attachment_with_the_same_name(
         self,
     ):
+        """
+        Test checks that overwrite method save() checks and delete an existing attachment with the same file name.
+        """
         task_attachment = TaskAttachment.objects.get(id=1)
-        task_attachment2 = TaskAttachment.objects.get(id=2)
+        self.test_task_attachment = TaskAttachment(
+            task=self.test_task,
+            attachment=SimpleUploadedFile("test_file.txt", b"content of test file"),
+        )
+        self.test_task_attachment.save()
         file_name = task_attachment.attachment
-        file_name2 = task_attachment2.attachment
+        file_name2 = self.test_task_attachment.attachment
 
         self.assertEqual(file_name, file_name2)
 
     def test_should_get_correct_upload_path_for_attachment_file(self):
+        """
+        Test checks that attachment files are uploaded on the right path.
+        """
         expected_upload_path = "attachments/tasks/1/test_file.txt"
         task_attachment = TaskAttachment.objects.get(id=1)
         actual_upload_path = task_attachment.attachment
@@ -114,6 +155,9 @@ class TestTaskAttachmentModel(TestCase):
         self.assertEqual(expected_upload_path, actual_upload_path)
 
     def test_should_return_correct_text_as_represenation_of_object(self):
+        """
+        Test check that the string representation of instance of object Task has correct text.
+        """
         expected_represenation = "<TaskAttachment id=1, attachment=attachments/tasks/1/test_file.txt, task_id=1>"
         task_attachment = TaskAttachment.objects.get(id=1)
         actual_representation = repr(task_attachment)
@@ -121,6 +165,9 @@ class TestTaskAttachmentModel(TestCase):
         self.assertEqual(expected_represenation, actual_representation)
 
     def test_should_return_correct_string_representation(self):
+        """
+        Test check that the string representation of instance of object Task has correct text.
+        """
         expected_string = (
             "Attachment: attachments/tasks/1/test_file.txt for Task: Test title"
         )
