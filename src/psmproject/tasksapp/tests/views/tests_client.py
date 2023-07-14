b8274f7c10ecdf0c676b23f445122855e0776d92
Adam <adamjawor@gmail.com>
2023-07-14 21:07:46 +0200

    [DEV-64] Update test for common and client with implementation of FactoryBoy

--- src/psmproject/tasksapp/tests/views/tests_client.py
+++ src/psmproject/tasksapp/tests/views/tests_client.py
@@ -1,39 +1,24 @@
-from django.contrib.auth.models import User
 from django.test import Client, TestCase
 from django.urls import reverse
 from tasksapp.models import Task
+from tasksapp.tests.views.factories import TaskFactory, UserFactory
 
 client = Client()
 
 
-class TestTaskListBaseView(TestCase):
+class TestClientTaskListBaseView(TestCase):
     def setUp(self) -> None:
         super().setUp()
-        self.client.login(username="test1", password="secret")
+        self.client.login(username=self.user.username, password="secret")
         self.response = self.client.get(reverse("tasks-client-list"))
 
     @classmethod
     def setUpClass(cls) -> None:
         super().setUpClass()
 
-        cls.test_user1 = User.objects.create_user(
-            username="test1", email="test1@test1.pl", password="secret"
-        )
-
-        cls.test_task1 = Task.objects.create(
-            title="Test title",
-            description="Test description",
-            realization_time="2023-12-01",
-            budget=1200.12,
-            client=cls.test_user1,
-        )
-        cls.test_task2 = Task.objects.create(
-            title="Second title",
-            description="New Test description2",
-            realization_time="2023-12-31",
-            budget=1000.12,
-            client=cls.test_user1,
-        )
+        cls.user = UserFactory()
+        cls.test_task1 = TaskFactory(client=cls.user)
+        cls.test_task2 = TaskFactory(client=cls.user)
 
     def test_should_return_status_code_200_when_request_by_name(self):
 
@@ -44,9 +29,6 @@ class TestTaskListBaseView(TestCase):
         self.assertEqual(self.response.status_code, 200)
         self.assertTemplateUsed(self.response, "tasksapp/tasks_list.html")
 
-    def test_should_return_correct_context_when_request_is_sent(self):
-        pass  # no contex in view
-
     def test_should_return_correct_objects_when_request_is_sent(self):
 
         self.assertEqual(self.response.status_code, 200)
@@ -56,18 +38,20 @@ class TestTaskListBaseView(TestCase):
             [self.test_task2, self.test_task1],
         )
 
+    def test_should_make_pagination_if_there_is_more_then_ten_element(self):
+        pass
+
     def test_should_redirect_if_not_logged_in(self):
         self.client.logout()
         self.response = self.client.get(reverse("tasks-client-list"))
-        self.assertRedirects(
-            self.response, "accounts/"
-        )  # check with allauth documentation
+        self.assertRedirects(self.response, f"/users/accounts/login/?next=/tasks/")
 
     def test_elements_should_be_sorted_by_id_from_newest(self):
-        pass
+
+        self.assertEqual(list(self.response.context["object_list"])[0], self.test_task2)
 
     def test_should_return_objects_filtered_by_phrase_in_task_title(self):
-        filter_word = "Second"
+        filter_word = self.test_task2.title[0:4]
         response_filter = self.client.get(
             reverse("tasks-client-list"), QUERY_STRING=f"q={filter_word}"
         )
@@ -77,7 +61,7 @@ class TestTaskListBaseView(TestCase):
         self.assertEqual(len(response_filter.context["object_list"]), 1)
 
     def test_should_return_objects_filterd_by_phrases_in_task_description(self):
-        filter_word = "New"
+        filter_word = self.test_task2.description[0:4]
         response_filter = self.client.get(
             reverse("tasks-client-list"), QUERY_STRING=f"q={filter_word}"
         )
@@ -87,19 +71,134 @@ class TestTaskListBaseView(TestCase):
         self.assertEqual(len(response_filter.context["object_list"]), 1)
 
 
-class TestTasksCurrentListView(TestCase):
-    pass
-
+class TestClientTasksCurrentListView(TestClientTaskListBaseView):
     def test_should_return_only_active_task(self):
-        pass
+        tasks_list = [self.test_task2, self.test_task1]
+
+        self.assertEqual(list(self.response.context["object_list"]), tasks_list)
+        self.assertEqual(len(self.response.context["object_list"]), 2)
 
 
-class TestTasksHistoricalListView(TestCase):
-    pass
+class TestClientTasksHistoricalListView(TestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.client.login(username=self.user.username, password="secret")
+        self.response = self.client.get(reverse("tasks-client-history-list"))
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        super().setUpClass()
+
+        cls.user = UserFactory()
+        cls.test_task1 = TaskFactory(client=cls.user)
+        cls.test_task2 = TaskFactory(client=cls.user)
+
+    def test_should_check_that_view_use_correct_template(self):
+
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, "tasksapp/tasks_history_list.html")
 
     def test_should_return_only_task_with_status_completed_and_cancelld(self):
-        pass
+        self.test_task2.status = 4
+        self.test_task2.save()
+        self.test_task2.refresh_from_db()
+
+        self.assertEqual(list(self.response.context["object_list"]), self.test_task2)
+        self.assertEqual(len(self.response.context["object_list"]), 1)
 
 
-class TestTaskCreateView(TestCase):
-    pass
+class TestClientTaskCreateView(TestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.client.login(username=self.user.username, password="secret")
+        self.client.force_login(self.user)
+        self.data = {
+            "title": "Task Title",
+            "description": "Task descrption",
+            "realization_time": "2023-12-31",
+            "budget": 1220.12,
+        }
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        super().setUpClass()
+        cls.user = UserFactory()
+
+    def test_should_return_status_code_200_when_request_is_sent(self):
+        response = client.get(reverse("task-create"))
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_should_create_task_object(self):
+        response = client.post(reverse("task-create"), data=self.data, follow=True)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Task.objects.filter(title=self.data["title"]).count(), 1)
+
+    def test_should_redirect_to_proper_url_after_success(self):
+        response = client.post(reverse("task-create"), data=self.data, follow=True)
+
+        self.assertRedirects(
+            response,
+            reverse("tasks-client-list"),
+            status_code=302,
+            target_status_code=200,
+            fetch_redirect_response=True,
+        )
+
+    def test_sholud_return_non_context_when_no_user_is_log_in(self):
+        self.client.logout()
+        response = client.get(reverse("task-create"))
+
+        self.assertRedirects(response, "/users/accounts/login/?next=/tasks/add/")
+
+
+class TestClientTaskEditView(TestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.client.login(username=self.user.username, password="secret")
+        self.client.force_login(self.user)
+        self.data = {
+            "title": "Task Title",
+            "description": "Task descrption",
+            "realization_time": "2023-12-31",
+            "budget": 1220.12,
+        }
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        super().setUpClass()
+        cls.user = UserFactory()
+        cls.test_task1 = TaskFactory(client=cls.user)
+
+    def test_should_return_status_code_302_when_request_is_sent(self):
+        response = self.client.post(
+            reverse("task-edit", kwargs={"pk": self.test_task1.id}),
+            data=self.data,
+        )
+
+        self.assertEqual(response.status_code, 302)
+
+    def test_should_update_existing_task_object(self):
+        response = self.client.post(
+            reverse("task-edit", kwargs={"pk": self.test_task1.id}),
+            data=self.data,
+        )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(
+            response, reverse("task-detail", kwargs={"pk": self.test_task1.id})
+        )
+        self.test_task1.refresh_from_db()
+        self.assertEqual(self.test_task1.title, self.data["title"])
+        self.assertEqual(self.test_task1.budget, self.data["budget"])
+
+    def test_sholud_return_non_context_when_no_user_is_log_in(self):
+        self.client.logout()
+        response = client.get(
+            reverse("task-edit", kwargs={"pk": self.test_task1.id}), follow=True
+        )
+
+        self.assertRedirects(
+            response, f"/users/accounts/login/?next=/tasks/edit/{self.test_task1.id}/"
+        )
--- src/psmproject/tasksapp/tests/views/tests_common.py
+++ src/psmproject/tasksapp/tests/views/tests_common.py
@@ -1,77 +1,133 @@
-from django.contrib.auth.models import User
 from django.test import Client, TestCase
 from django.urls import reverse
-from tasksapp.models import Task
+from tasksapp.tests.views.factories import (
+    TaskAttachmentFactory,
+    TaskFactory,
+    UserFactory,
+)
 
 client = Client()
 
 
-class TestTaskDetailView(TestCase):
+class TestCommonTaskDetailView(TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        super().setUpClass()
+        super().setUpClass()
+
+        cls.user = UserFactory()
+        cls.test_task1 = TaskFactory(client=cls.user)
+        cls.test_task2 = TaskFactory(client=cls.user)
+        cls.test_attachment1 = TaskAttachmentFactory(task=cls.test_task1)
+        cls.test_attachment2 = TaskAttachmentFactory(task=cls.test_task2)
+
+    def test_should_return_status_code_200_when_request_by_name(self):
+        self.client.login(username=self.user.username, password="secret")
+        response = self.client.get(
+            reverse("task-detail", kwargs={"pk": self.test_task1.id})
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_should_check_that_views_use_correct_template(self):
+        self.client.login(username=self.user.username, password="secret")
+        response = self.client.get(
+            reverse("task-detail", kwargs={"pk": self.test_task1.id})
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "tasksapp/task_detail.html")
+
+    def test_should_return_correct_context_when_request_is_sent(self):
+        self.client.login(username=self.user.username, password="secret")
+        response = self.client.get(
+            reverse("task-detail", kwargs={"pk": self.test_task1.id})
+        )
+        self.assertIn("attachments", response.context)
+        self.assertIn("task", response.context)
+
+    def test_should_return_correct_objects_when_request_is_sent(self):
+        self.client.login(username=self.user.username, password="secret")
+        response = self.client.get(
+            reverse("task-detail", kwargs={"pk": self.test_task1.id})
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "tasksapp/task_detail.html")
+        self.assertEqual(response.context["task"], self.test_task1)
+
+    def test_should_redirect_if_not_logged_in(self):
+        self.client.logout()
+        response = self.client.get(
+            reverse("task-detail", kwargs={"pk": self.test_task1.id})
+        )
+        self.assertRedirects(
+            response, f"/users/accounts/login/?next=/tasks/{self.test_task1.id}"
+        )
+
+
+class TestCommonTaskDeleteView(TestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.client.login(username=self.user.username, password="secret")
+
     @classmethod
     def setUpClass(cls) -> None:
         super().setUpClass()
 
-        cls.test_user1 = User.objects.create_user(
-            username="test1", email="test1@test1.pl", password="secret"
-        )
-
-        cls.test_task1 = Task.objects.create(
-            title="Test title",
-            description="Test description",
-            realization_time="2023-12-01",
-            budget=1200.12,
-            client=cls.test_user1,
-        )
-        cls.test_task2 = Task.objects.create(
-            title="Second title",
-            description="Test description2",
-            realization_time="2023-12-31",
-            budget=1000.12,
-            client=cls.test_user1,
-        )
+        cls.user = UserFactory()
+        cls.user2 = UserFactory()
+        cls.test_task1 = TaskFactory(client=cls.user)
+        cls.test_task2 = TaskFactory(client=cls.user)
+        cls.test_task3 = TaskFactory(client=cls.user)
+        cls.test_attachment1 = TaskAttachmentFactory(task=cls.test_task1)
+        cls.test_attachment2 = TaskAttachmentFactory(task=cls.test_task2)
 
     def test_should_return_status_code_200_when_request_by_name(self):
-        self.client.login(username="test1", password="secret")
-        self.response = self.client.get(
-            reverse("task-detail", kwargs={"pk": self.test_task1.id})
+        response = self.client.get(
+            reverse("task-delete", kwargs={"pk": self.test_task1.id})
         )
 
-        self.assertEqual(self.response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
 
     def test_should_check_that_views_use_correct_template(self):
-        self.client.login(username="test1", password="secret")
-        self.response = self.client.get(
-            reverse("task-detail", kwargs={"pk": self.test_task1.id})
+        response = self.client.get(
+            reverse("task-delete", kwargs={"pk": self.test_task1.id})
         )
 
-        self.assertEqual(self.response.status_code, 200)
-        self.assertTemplateUsed(self.response, "tasksapp/task_detail.html")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "tasksapp/task_confirm_delete.html")
 
-    def test_should_return_correct_context_when_request_is_sent(self):
-        self.client.login(username="test1", password="secret")
-        self.response = self.client.get(
-            reverse("task-detail", kwargs={"pk": self.test_task1.id})
+    def test_should_delete_task_as_client_that_is_creator_of_task(self):
+        response = self.client.post(
+            reverse("task-delete", kwargs={"pk": self.test_task1.id}), follow=True
         )
-        # self.assertIn("attachments", self.response.context) add TaskAttachment object relateted to test_task1
-        self.assertIn("task", self.response.context)
 
-    """
-    def test_should_return_correct_objects_when_request_is_sent(self):
+        self.assertEqual(response.status_code, 302)
+        self.assertIsNone(self.test_task1)
+        self.assertRedirects(response, reverse("tasks-client-list"))
 
-        self.assertEqual(self.response.status_code, 200)
-        self.assertTemplateUsed(self.response, "tasksapp/tasks_list.html")
-        self.assertEqual(list(self.response.context["object_list"]),[self.test_task2, self.test_task1])
-        """
+    def test_should_delete_task_as_moderator(self):
+        pass  # should be tested after implementation of gropus for users
 
-    def test_should_redirect_if_not_logged_in(self):
-        self.client.logout()
-        self.response = self.client.get(
-            reverse("task-detail", kwargs={"pk": self.test_task1.id})
+    def test_sholud_block_user_acces_if_is_not_creator_of_task(self):
+        client.logout()
+        client.login(username=self.user2.username, password="secret")
+        response = self.client.post(
+            reverse("task-delete", kwargs={"pk": self.test_task2.id}), follow=True
         )
-        self.assertRedirects(
-            self.response, "accounts/"
-        )  # check with allauth documentation
 
+        self.assertEqual(response.status_code, 403)
+        self.assertIsNotNone(self.test_task2)
 
-class TestTaskDeleteView(TestCase):
-    pass
+    def test_should_delete_task_if_it_has_status_canceled(self):
+        self.test_task3.status = 5
+        self.test_task3.save()
+        self.test_task3.refresh_from_db()
+        response = self.client.post(
+            reverse("task-delete", kwargs={"pk": self.test_task3.id}), follow=True
+        )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertIsNone(self.test_task3)
+        self.assertRedirects(response, reverse("tasks-client-list"))
