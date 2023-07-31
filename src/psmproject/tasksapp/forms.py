150b29609e5b6b03b8756ec86fb5558a678b8f47
Bartosz Wisniewski <bartekwisnia@op.pl>
2023-07-31 16:08:04 +0200

    [DEV-71] Task attachment verification (#24)
    * [DEV-71] Task attachment verification
    
    * [DEV-71] Task attachment verification - black
    
    * [DEV-71] corrected ValidationError import in tasksapp models

--- src/psmproject/tasksapp/forms.py
+++ src/psmproject/tasksapp/forms.py
@@ -1,4 +1,5 @@
-from django.forms import DateInput, HiddenInput, ModelForm
+from django.forms import DateInput, HiddenInput, ModelForm, ValidationError
+from django.template.defaultfilters import filesizeformat
 
 from .models import Task, TaskAttachment
 
@@ -40,3 +41,19 @@ class TaskAttachmentForm(ModelForm):
         model = TaskAttachment
         fields = "__all__"
         widgets = {"task": HiddenInput()}
+
+    def clean_attachment(self):
+        """
+        Custom clean method to verify attachment file properties. Conditions are defined in a model.
+        Properties checked:
+        1. File type
+        2. File size
+        """
+        attachment = self.cleaned_data["attachment"]
+        if attachment.content_type in TaskAttachment.CONTENT_TYPES:
+            if attachment.size > TaskAttachment.MAX_UPLOAD_SIZE:
+                error_message = f"File too big. Max file size: {filesizeformat(TaskAttachment.MAX_UPLOAD_SIZE)}"
+                raise ValidationError(error_message)
+        else:
+            raise ValidationError("File type is not supported")
+        return attachment
--- src/psmproject/tasksapp/models.py
+++ src/psmproject/tasksapp/models.py
@@ -74,6 +74,9 @@ class TaskAttachment(models.Model):
     """
 
     MAX_ATTACHMENTS = 10
+    ALLOWED_EXTENSIONS = (".txt", ".pdf")
+    CONTENT_TYPES = ("text/plain", "application/pdf")
+    MAX_UPLOAD_SIZE = 10485760  # 10MB
 
     task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name="attachments")
     attachment = models.FileField(upload_to=get_upload_path)
@@ -92,9 +95,15 @@ class TaskAttachment(models.Model):
 
     def clean(self):
         """
-        Custom clean method that checks the number of attachments for the related task and
-        throws an error if the limit is exceeded.
+        Custom clean method that checks:
+        1. File extension
+        2. If the number of attachments for the related task does not exceed limit
+        throws an error if any of the conditions is invalid
         """
+        if not self.attachment:
+            return
+        if not str(self.attachment).endswith(TaskAttachment.ALLOWED_EXTENSIONS):
+            raise ValidationError("File type not allowed")
         existing_attachments = TaskAttachment.objects.filter(task=self.task).count()
         if existing_attachments >= TaskAttachment.MAX_ATTACHMENTS:
             print("here")
