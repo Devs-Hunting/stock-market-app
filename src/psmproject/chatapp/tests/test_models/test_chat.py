75b627ff58572a28f0bcc1f7f1b5160ecedf926f
ndestrieux <n.destrieux@gmail.com>
2023-08-02 20:15:45 +0200

    - Create method overridden for Chat model manager, tests adapted accordingly. - Factory boy library used for tests

--- src/psmproject/chatapp/tests/test_models/test_chat.py
+++ src/psmproject/chatapp/tests/test_models/test_chat.py
@@ -1,37 +1,23 @@
 from chatapp.models import Chat
-from django.contrib.auth import get_user_model
-from django.contrib.contenttypes.models import ContentType
 from django.test import TestCase
-from tasksapp.models import Task
+from factories.factories import TaskFactory
 
 
 class TestChatModel(TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         super().setUpClass()
-        cls.test_user = get_user_model().objects.create_user(
-            username="test@test.pl", password="secret"
-        )
-        cls.test_task = Task.objects.create(
-            title="Test title",
-            description="Test description",
-            realization_time="2023-12-01",
-            budget=10.00,
-            client=cls.test_user,
-        )
-        cls.test_task_content_type = ContentType.objects.get_for_model(cls.test_task)
-        cls.test_task_chat = Chat.objects.create(
-            content_type=cls.test_task_content_type, object_id=cls.test_task.id
-        )
-        cls.test_user_content_type = ContentType.objects.get_for_model(cls.test_user)
-        cls.test_user_chat = Chat.objects.create(
-            content_type=cls.test_user_content_type, object_id=cls.test_user.id
-        )
+        cls.test_task = TaskFactory()
+        cls.test_task_chat = Chat.objects.create(obj=cls.test_task)
+        cls.test_private_chat = Chat.objects.create()
 
     def testShouldReturnTrueWhenTaskChatInstanceCreatedInDatabase(self):
         chats = Chat.objects.all()
         self.assertIn(self.test_task_chat, chats)
 
-    def testShouldReturnTrueWhenUserChatInstanceCreatedInDatabase(self):
-        users = Chat.objects.all()
-        self.assertIn(self.test_user_chat, users)
+    def testShouldReturnTrueWhenTaskIsProperlyLinkedWithCreatedTaskChat(self):
+        self.assertEqual(self.test_task_chat.content_object, self.test_task)
+
+    def testShouldReturnTrueWhenPrivateChatInstanceCreatedInDatabase(self):
+        chats = Chat.objects.all()
+        self.assertIn(self.test_private_chat, chats)
