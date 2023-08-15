f96288d7fda35c8fcb118b18a2f4b042930dcdb6
Bartosz Wisniewski <bartekwisnia@op.pl>
2023-08-15 18:00:14 +0200

    [DEV-72] JS scripts in task form refactored

--- src/psmproject/tasksapp/static/tasksapp/scripts-task-form.js
+++ src/psmproject/tasksapp/static/tasksapp/scripts-task-form.js
@@ -1,89 +1,86 @@
-const skills = JSON.parse(document.getElementById('skills').textContent);
+class SkillManager {
+    constructor() {
+        this.skills = JSON.parse(document.getElementById('skills').textContent);
+        this.taskSkillPrefix = JSON.parse(document.getElementById('skillPrefix').textContent);
+        this.skillPrefix = "pos-skill-";
+        this.filterInput = document.getElementById('filter-skills');
+        this.taskSkillList = document.getElementById("task-skills-list");
+        this.skillList = document.getElementById("pos-skills-list");
+        this.initEventListeners();
+    }
 
-const taskSkillPrefix = JSON.parse(document.getElementById('skillPrefix').textContent);
-const skillPrefix = "pos-skill-";
-
-const filterInput = document.getElementById('filter-skills');
-const taskSkillList = document.getElementById("task-skills-list");
-const skillList = document.getElementById("pos-skills-list");
-
-
-function renderSkillForm(skill, idPrefix, onClick, parent){
+    renderSkillForm(skill, idPrefix, onClick, parent) {
         const skillButton = document.createElement("button");
-        skillButton.id = idPrefix+skill.id;
+        skillButton.id = idPrefix + skill.id;
         skillButton.innerHTML = skill.skill;
         skillButton.onclick = onClick;
-        skillButton.classList.add("btn");
-        skillButton.classList.add("btn-primary");
-        skillButton.classList.add("btn-sm");
-        skillButton.classList.add("ms-1");
+        skillButton.classList.add("btn", "btn-primary", "btn-sm", "ms-1");
 
         const skillInput = document.createElement("input");
-        skillInput.name = idPrefix+skill.id;
+        skillInput.name = idPrefix + skill.id;
         skillInput.type = "hidden";
         skillInput.value = skill.skill;
 
         skillButton.appendChild(skillInput);
         parent.appendChild(skillButton);
-}
+    }
 
-function renderSkill(skill, idPrefix, onClick, parent){
+    renderSkill(skill, idPrefix, onClick, parent) {
         const skillSpan = document.createElement("span");
-        skillSpan.id = idPrefix+skill.id;
-        skillSpan.classList.add("badge");
-        skillSpan.classList.add("text-bg-primary");
-        skillSpan.classList.add("ms-1");
+        skillSpan.id = idPrefix + skill.id;
+        skillSpan.classList.add("badge", "text-bg-primary", "ms-1");
         skillSpan.innerHTML = skill.skill;
         skillSpan.onclick = onClick;
         parent.appendChild(skillSpan);
-}
+    }
 
-function removeSkill(skill, idPrefix, parent){
-    const skill_el = document.getElementById(idPrefix+skill.id);
-    parent.removeChild(skill_el);
-}
+    removeSkill(skill, idPrefix, parent) {
+        const skill_el = document.getElementById(idPrefix + skill.id);
+        parent.removeChild(skill_el);
+    }
 
-function selectSkill(skill){
-    renderSkillForm(skill, taskSkillPrefix, ()=>deselectSkill(skill), taskSkillList);
-    removeSkill(skill, skillPrefix, skillList);
-    const index = skills.findIndex(item => item.id === skill.id);
-    skills.splice(index, 1);
-}
+    selectSkill(skill) {
+        this.renderSkillForm(skill, this.taskSkillPrefix, () => this.deselectSkill(skill), this.taskSkillList);
+        this.removeSkill(skill, this.skillPrefix, this.skillList);
+        const index = this.skills.findIndex(item => item.id === skill.id);
+        this.skills.splice(index, 1);
+    }
 
-function deselectSkill(skill){
-    removeSkill(skill, taskSkillPrefix, taskSkillList);
-    skills.push(skill);
-    displayList(skills, filterInput.value)
-}
+    deselectSkill(skill) {
+        this.removeSkill(skill, this.taskSkillPrefix, this.taskSkillList);
+        this.skills.push(skill);
+        this.displayList(this.skills, this.filterInput.value);
+    }
 
-function displayList(skills, filter){
-    skillList.innerHTML = '';
-    if (filter.length > 0) {
-        const results = skills.filter(obj => {
-            return obj.skill.toLowerCase().includes(filter.toLowerCase());
-        });
+    displayList(skills, filter) {
+        this.skillList.innerHTML = '';
+        if (filter.length > 0) {
+            const results = skills.filter(obj => {
+                return obj.skill.toLowerCase().includes(filter.toLowerCase());
+            });
 
-        for (const skill of results) {
-            renderSkill(skill, skillPrefix, ()=>selectSkill(skill), skillList);
+            for (const skill of results) {
+                this.renderSkill(skill, this.skillPrefix, () => this.selectSkill(skill), this.skillList);
+            }
         }
     }
-}
 
-function addOnClickToExistingSkills(parent){
-    const buttons_el = parent.getElementsByTagName("button");
-    const buttons_el_arr = [ ...buttons_el ];
-    for (const button of buttons_el_arr) {
-        const input = button.getElementsByTagName("input")[0];
-        const skill_obj = {"id": button.id.replace(taskSkillPrefix, ""), "skill": input.value}
-        button.onclick = () => deselectSkill(skill_obj);
+    addOnClickToExistingSkills(parent) {
+        const buttons_el = parent.getElementsByTagName("button");
+        const buttons_el_arr = [...buttons_el];
+        for (const button of buttons_el_arr) {
+            const input = button.getElementsByTagName("input")[0];
+            const skill_obj = { "id": button.id.replace(this.taskSkillPrefix, ""), "skill": input.value }
+            button.onclick = () => this.deselectSkill(skill_obj);
+        }
+    }
+
+    initEventListeners() {
+        this.addOnClickToExistingSkills(this.taskSkillList);
+        this.filterInput.addEventListener('input', (e) => {
+            this.displayList(this.skills, e.target.value);
+        });
     }
 }
 
-addOnClickToExistingSkills(taskSkillList);
-
-
-const filterHandler = function(e) {
-    displayList(skills, e.target.value);
-}
-
-filterInput.addEventListener('input', filterHandler);
+const skillManager = new SkillManager();
