e5501b886f75e37cccf051cad1ce595ad1fbcb1d
Bartosz Wisniewski <bartekwisnia@op.pl>
2023-08-16 09:20:01 +0200

    [DEV-72] JSDocs for skill scripts

--- src/psmproject/tasksapp/static/tasksapp/scripts-task-form.js
+++ src/psmproject/tasksapp/static/tasksapp/scripts-task-form.js
@@ -1,4 +1,8 @@
+/** Class used to store variables and methods for managing skills on edit and create templates. */
 class SkillManager {
+    /**
+     * Create skill manager, init internal variables and link variables with document elements
+     */
     constructor() {
         this.skills = JSON.parse(document.getElementById('skills').textContent);
         this.taskSkillPrefix = JSON.parse(document.getElementById('skillPrefix').textContent);
@@ -9,6 +13,10 @@ class SkillManager {
         this.initEventListeners();
     }
 
+    /**
+     * Prepare and append document element for single skill input button that already exists in the parent object
+     * (Task) and is part of the form. Creates html tags, adds classes and contents
+     */
     renderSkillForm(skill, idPrefix, onClick, parent) {
         const skillButton = document.createElement("button");
         skillButton.id = idPrefix + skill.id;
@@ -25,6 +33,10 @@ class SkillManager {
         parent.appendChild(skillButton);
     }
 
+    /**
+     * Prepare and append document element for single skill on selection list. Creates html tags, adds classes
+     * and contents
+     */
     renderSkill(skill, idPrefix, onClick, parent) {
         const skillSpan = document.createElement("span");
         skillSpan.id = idPrefix + skill.id;
@@ -34,11 +46,17 @@ class SkillManager {
         parent.appendChild(skillSpan);
     }
 
+    /**
+     * Remove skill from parent element
+     */
     removeSkill(skill, idPrefix, parent) {
         const skill_el = document.getElementById(idPrefix + skill.id);
         parent.removeChild(skill_el);
     }
 
+    /**
+     * Select skill and add it to the parent object (Task). Method removes it also from selection list.
+     */
     selectSkill(skill) {
         this.renderSkillForm(skill, this.taskSkillPrefix, () => this.deselectSkill(skill), this.taskSkillList);
         this.removeSkill(skill, this.skillPrefix, this.skillList);
@@ -46,12 +64,18 @@ class SkillManager {
         this.skills.splice(index, 1);
     }
 
+    /**
+     * Remove skill from the parent object (Task). Method also updates selection list after removal from form.
+     */
     deselectSkill(skill) {
         this.removeSkill(skill, this.taskSkillPrefix, this.taskSkillList);
         this.skills.push(skill);
         this.displayList(this.skills, this.filterInput.value);
     }
 
+    /**
+     * Display list of all skills matching filter criteria.
+     */
     displayList(skills, filter) {
         this.skillList.innerHTML = '';
         if (filter.length > 0) {
@@ -65,6 +89,9 @@ class SkillManager {
         }
     }
 
+    /**
+     * Add onClick action to all skills existing on form. They are generate by the Django template without actions.
+     */
     addOnClickToExistingSkills(parent) {
         const buttons_el = parent.getElementsByTagName("button");
         const buttons_el_arr = [...buttons_el];
@@ -75,6 +102,9 @@ class SkillManager {
         }
     }
 
+    /**
+     * Init event listeners for document elements
+     */
     initEventListeners() {
         this.addOnClickToExistingSkills(this.taskSkillList);
         this.filterInput.addEventListener('input', (e) => {
