46be0761924601e0a48061e6cd593fe041f97689
Bartosz Wisniewski <bartekwisnia@op.pl>
2023-08-21 15:43:03 +0200

    removed ready method from UsersappConfig

--- src/psmproject/usersapp/apps.py
+++ src/psmproject/usersapp/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class UsersappConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "usersapp"
-
-    def ready(self):
-        pass
