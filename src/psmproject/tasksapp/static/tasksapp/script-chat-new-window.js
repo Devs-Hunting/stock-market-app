de12a735194e61ca3c13285abfdee830f2c25203
ndestrieux <n.destrieux@gmail.com>
2023-08-24 18:04:41 +0200

    New chat window parameters and chat elements height and width fixed.

--- src/psmproject/tasksapp/static/tasksapp/script-chat-new-window.js
+++ src/psmproject/tasksapp/static/tasksapp/script-chat-new-window.js
@@ -4,9 +4,6 @@ function onClick()  {
     const chat_id = chat_element.getAttribute("chat_id")
     newWindow = window.open(href,
     "Chat " + chat_id,
-    "width=500, height=700",
-    toolbar=0,
-    statusbar=0,
-    locationbar=0,
+    "width=500, height=" + window.screen.height
     );
 }
