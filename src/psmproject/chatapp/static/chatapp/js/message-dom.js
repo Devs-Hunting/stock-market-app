2868906e9cdb35f871ea860f90fa477de0077bec
ndestrieux <n.destrieux@gmail.com>
2023-08-26 23:34:10 +0200

    - validation set to avoid submitting empty messages - letter count added - js code reviewed, rearranged in classes when necessary and splitted in several files

---
+++ src/psmproject/chatapp/static/chatapp/js/message-dom.js
@@ -0,0 +1,22 @@
+export class NewMessage    {
+    /**
+    * create html template for message display in chat depending on current logged user and author
+    */
+    constructor(data)   {
+        this.data = data;
+        this.currentUserMessageTemplate = document.querySelector("#current-user-message-template");
+        this.otherUserMessageTemplate = document.querySelector("#other-user-message-template");
+    }
+    create(currentUser)    {
+        let newMessageElement = document.createElement("div");
+        if (this.data.author == currentUser) {
+            newMessageElement.innerHTML = this.currentUserMessageTemplate.innerHTML;
+        } else  {
+            newMessageElement.innerHTML = this.otherUserMessageTemplate.innerHTML;
+            newMessageElement.querySelector(".author").textContent = this.data.author + " :"
+        };
+        newMessageElement.querySelector(".message").textContent = this.data.message;
+        newMessageElement.querySelector("img").setAttribute("src", this.data.picture);
+        return newMessageElement;
+    }
+};
--- src/psmproject/chatapp/views.py
+++ src/psmproject/chatapp/views.py
@@ -1,7 +1,12 @@
-from chatapp.models import Chat
+from chatapp.models import Chat, Message
 from django.views.generic import DetailView
 
 
 class ChatView(DetailView):
     model = Chat
     template_name = "chatapp/chat_room.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["message_max_length"] = Message._meta.get_field("content").max_length
+        return context
