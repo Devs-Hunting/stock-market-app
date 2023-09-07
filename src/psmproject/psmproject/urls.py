8e3c972f284e940a8a36f59eb14e3058be341e03
ndestrieux <n.destrieux@gmail.com>
2023-09-07 07:31:41 +0200

    Media files related implementations rolledback as it will be implemented in another branch

--- src/psmproject/psmproject/urls.py
+++ src/psmproject/psmproject/urls.py
@@ -14,8 +14,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import include, path
 
 urlpatterns = [
@@ -24,7 +22,3 @@ urlpatterns = [
     path("tasks/", include("tasksapp.urls")),
     path("chat/", include("chatapp.urls")),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
