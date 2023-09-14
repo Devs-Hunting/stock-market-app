d7fd623e54bf2a802e1a89f18a03c08f2f872336
Rafal G <rafalgbc@gmail.com>
2023-09-14 23:52:13 +0200

    [DEV-36] Update dockerfile for local deployment and add fixtuters to the project.

--- src/psmproject/psmproject/settings/base.py
+++ src/psmproject/psmproject/settings/base.py
@@ -150,9 +150,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATIC_ROOT = BASE_DIR / "static"
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 
 # Media files
 MEDIA_FILES = "/media/"
