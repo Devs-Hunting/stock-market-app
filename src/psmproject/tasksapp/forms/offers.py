07d29ede0f00a82506a0da9eba0e5d53111879c2
Bartosz Wisniewski <bartekwisnia@op.pl>
2023-10-07 20:28:35 +0200

    [DEV-91] change all forms to crispy bootstrap5 (#44)
    * [DEV-91] offer, task and task search forms replaced with crispy
    
    * [DEV-91] offer, task and task search forms replaced with crispy
    
    * [DEV-91] black

--- src/psmproject/tasksapp/forms/offers.py
+++ src/psmproject/tasksapp/forms/offers.py
@@ -1,3 +1,6 @@
+from crispy_forms.bootstrap import InlineField
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Div, Layout
 from django import forms
 
 from ..models import Offer
@@ -8,16 +11,27 @@ class DateInput(forms.DateInput):
 
 
 class TaskSearchForm(forms.Form):
-    template_name = "tasksapp/form_snippet_horizontal.html"
-
     query = forms.CharField(label="Search", max_length=100, required=False)
     realization_time = forms.DateField(widget=DateInput(), required=False)
     budget = forms.DecimalField(max_digits=6, decimal_places=2, required=False)
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+        self.helper.form_class = "form-inline"
+        self.helper.field_template = "bootstrap5/layout/inline_field.html"
+        self.helper.layout = Layout(
+            Div(
+                InlineField("query", wrapper_class="col"),
+                InlineField("realization_time", wrapper_class="col"),
+                InlineField("budget", wrapper_class="col"),
+                css_class="row mb-3 align-items-center",
+            )
+        )
+
 
 class OfferForm(forms.ModelForm):
-    template_name = "tasksapp/form_snippet.html"
-
     class Meta:
         model = Offer
         fields = ["description", "realization_time", "budget"]
@@ -25,15 +39,11 @@ class OfferForm(forms.ModelForm):
 
 
 class OfferModeratorForm(forms.ModelForm):
-    template_name = "tasksapp/form_snippet.html"
-
     class Meta:
         model = Offer
         fields = ["description"]
 
 
 class OfferSearchForm(forms.Form):
-    template_name = "tasksapp/form_snippet_horizontal.html"
-
     query = forms.CharField(label="Search", max_length=100, min_length=3, required=False)
     accepted = forms.BooleanField(required=False)
