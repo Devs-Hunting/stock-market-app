572a47c5c680bb9b4649607f0ae7accf7fe2bbec
ndestrieux <n.destrieux@gmail.com>
2023-11-12 20:55:19 +0100

    Tests for OpenPrivateChatView created.

--- src/psmproject/chatapp/tests/test_views.py
+++ src/psmproject/chatapp/tests/test_views.py
@@ -1,6 +1,8 @@
+from chatapp.models import PrivateChat
+from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
 from django.test import TestCase
 from django.urls import reverse
-from factories.factories import ChatFactory, ChatParticipantFactory
+from factories.factories import ChatFactory, ChatParticipantFactory, UserFactory
 
 
 class ChatViewTest(TestCase):
@@ -26,3 +28,50 @@ class ChatViewTest(TestCase):
         self.client.login(username=self.other_chat_participant.user.username, password="secret")
         response = self.client.get(reverse("chat", kwargs={"pk": self.chat.id}))
         self.assertEqual(response.status_code, 403)
+
+
+class OpenPrivateChatViewTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.private_chat = ChatFactory()
+        cls.participant_1 = ChatParticipantFactory(chat=cls.private_chat)
+        cls.participant_2 = ChatParticipantFactory(chat=cls.private_chat)
+        cls.participant_3 = ChatParticipantFactory()
+
+    def test_should_return_correct_url_when_opening_existing_private_chat(self):
+        self.client.login(username=self.participant_1.user.username, password="secret")
+        response = self.client.get(reverse("open-chat", kwargs={"user_id": self.participant_2.user.id}))
+        self.assertRedirects(response, reverse("chat", kwargs={"pk": self.private_chat.id}))
+
+    def test_should_create_new_private_chat_and_return_correct_url_when_private_chat_does_exist(self):
+        self.client.login(username=self.participant_1.user.username, password="secret")
+        chat_before_request = (
+            PrivateChat.objects.filter(participants__user=self.participant_1.user)
+            .filter(participants__user=self.participant_3.user)
+            .first()
+        )
+        self.assertIsNone(chat_before_request)
+        response = self.client.get(reverse("open-chat", kwargs={"user_id": self.participant_3.user.id}))
+        chat_after_request = (
+            PrivateChat.objects.filter(participants__user=self.participant_1.user)
+            .filter(participants__user=self.participant_3.user)
+            .first()
+        )
+        self.assertRedirects(response, reverse("chat", kwargs={"pk": chat_after_request.id}))
+
+    def test_should_return_error_when_one_participant_does_not_exist_and_does_not_create_new_chat(self):
+        new_user = UserFactory()
+        self.client.login(username=new_user.username, password="secret")
+        with self.assertRaises(ObjectDoesNotExist):
+            self.client.get(reverse("open-chat", kwargs={"user_id": 99}))
+        chat = PrivateChat.objects.filter(participants__user=new_user).first()
+        self.assertIsNone(chat)
+
+    def test_should_return_error_when_error_on_new_chat_creation(self):
+        self.client.login(username=self.participant_1.user.username, password="secret")
+        duplicate_private_chat = ChatFactory()
+        other_participant_1 = ChatParticipantFactory(chat=duplicate_private_chat, user=self.participant_1.user)
+        other_participant_2 = ChatParticipantFactory(chat=duplicate_private_chat, user=self.participant_2.user)
+        with self.assertRaises(MultipleObjectsReturned):
+            self.client.get(reverse("open-chat", kwargs={"user_id": other_participant_2.user.id}))
