a8822557ed709a532b2bc6911c3c97a6e8d970e7
Adam <adamjawor@gmail.com>
2023-12-03 16:17:23 +0100

    [DEV-104] refactror attachment views for complaint and task

--- src/psmproject/tasksapp/forms/complaint.py
+++ src/psmproject/tasksapp/forms/complaint.py
@@ -1,9 +1,35 @@
 from django import forms
+from django.template.defaultfilters import filesizeformat
 
-from ..models import Complaint
+from ..models import Complaint, ComplaintAttachment
 
 
 class ComplaintForm(forms.ModelForm):
     class Meta:
         model = Complaint
         fields = ["content"]
+
+
+class ComplaintAttachmentForm(forms.ModelForm):
+    template_name = "tasksapp/form_snippet.html"
+
+    class Meta:
+        model = ComplaintAttachment
+        fields = "__all__"
+        widgets = {"complaint": forms.HiddenInput()}
+
+    def clean_attachment(self):
+        """
+        Custom clean method to verify attachment file properties. Conditions are defined in a model.
+        Properties checked:
+        1. File type
+        2. File size
+        """
+        attachment = self.cleaned_data["attachment"]
+        if attachment.content_type in ComplaintAttachment.CONTENT_TYPES:
+            if attachment.size > ComplaintAttachment.MAX_UPLOAD_SIZE:
+                error_message = f"File too big. Max file size: {filesizeformat(ComplaintAttachment.MAX_UPLOAD_SIZE)}"
+                raise forms.ValidationError(error_message)
+        else:
+            raise forms.ValidationError("File type is not supported")
+        return attachment
--- src/psmproject/tasksapp/tests/views/tests_attachment.py
+++ src/psmproject/tasksapp/tests/views/tests_attachment.py
@@ -6,8 +6,14 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import Client, TestCase
 from django.urls import reverse
-from factories.factories import TaskAttachmentFactory, TaskFactory, UserFactory
-from tasksapp.models import ATTACHMENTS_PATH, TaskAttachment
+from factories.factories import (
+    ComplaintAttachmentFactory,
+    ComplaintFactory,
+    TaskAttachmentFactory,
+    TaskFactory,
+    UserFactory,
+)
+from tasksapp.models import ATTACHMENTS_PATH, ComplaintAttachment, TaskAttachment
 
 
 class TestTaskAttachmentAddView(TestCase):
@@ -281,6 +287,256 @@ class TestTaskAttachmentDeleteView(TestCase):
 
         response = self.client.post(reverse("task-attachment-delete", kwargs={"pk": temp_attachment_id}))
         self.assertEqual(response.status_code, 404)
+
+
+class TestComplaintAttachmentAddView(TestCase):
+    """
+    Test case for add complaint attachment view
+    """
+
+    def setUp(self) -> None:
+        """
+        Set up method that is run before every individual test.
+        Here it prepares test user, complaint, complaint attachment.
+        """
+        super().setUp()
+        self.client = Client()
+        self.user = UserFactory.create()
+        self.test_complaint = ComplaintFactory.create(complainant=self.user)
+        self.attachment = SimpleUploadedFile("test_file.txt", b"content of test file")
+        self.client.login(username=self.user.username, password="secret")
+        self.data = {
+            "complaint": self.test_complaint.id,
+            "attachment": self.attachment,
+        }
+
+    def tearDown(self) -> None:
+        """
+        Clean up method after each test case. Deletes all created attachments files.
+        """
+        file_path = settings.MEDIA_ROOT / ATTACHMENTS_PATH
+        shutil.rmtree(file_path, ignore_errors=True)
+        super().tearDown()
+
+    def test_should_return_correct_status_code_when_request_is_sent(self):
+        """
+        Test checks that correct status code is returned.
+        """
+        response = self.client.get(reverse("complaint-add-attachment", kwargs={"pk": self.test_complaint.id}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_should_add_attachment_to_existing_complaint(self):
+        """
+        Test whether the complaint attachment is added to the complaint.
+        """
+        response = self.client.post(
+            reverse("complaint-add-attachment", kwargs={"pk": self.test_complaint.id}),
+            data=self.data,
+            follow=True,
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.test_complaint.refresh_from_db()
+        new_complaint_attachment = ComplaintAttachment.objects.get(complaint=self.test_complaint)
+        self.assertEqual(
+            str(new_complaint_attachment.attachment),
+            f"{ATTACHMENTS_PATH}complaints/{self.test_complaint.id}/{self.attachment}",
+        )
+        self.assertEqual(len(self.test_complaint.attachments.all()), 1)
+
+    def test_should_raise_exception_when_adding_attachment_to_non_existing_task(self):
+        """
+        Test case to check if it is not possible to add an attachment to non existing complaint.
+        """
+        with self.assertRaises(ObjectDoesNotExist):
+            self.client.post(
+                reverse("complaint-add-attachment", kwargs={"pk": 999}),
+                data={
+                    "task": 999,
+                    "attachment": self.attachment,
+                },
+                follow=True,
+            )
+
+    def test_should_block_when_add_attachment_to_existing_complaint_by_other_user(self):
+        """
+        Test checks that it is possible for another user to add attachment to existing complaint.
+        """
+        self.user2 = UserFactory.create()
+        self.client.force_login(self.user2)
+        response = self.client.post(
+            reverse("complaint-add-attachment", kwargs={"pk": self.test_complaint.id}),
+            data=self.data,
+            follow=True,
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.test_complaint.refresh_from_db()
+        with self.assertRaises(ComplaintAttachment.DoesNotExist):
+            ComplaintAttachment.objects.get(complaint=self.test_complaint)
+
+    def test_should_redirect_to_proper_url_after_success(self):
+        """
+        Test checks that the view redirects to the correct URL after successful add attachment.
+        """
+        response = self.client.post(
+            reverse("complaint-add-attachment", kwargs={"pk": self.test_complaint.id}),
+            data=self.data,
+            follow=True,
+        )
+
+        self.assertRedirects(response, reverse("complaint-detail", kwargs={"pk": self.test_complaint.id}))
+
+    def test_should_return_non_context_when_non_logged_in_user_is_trying_to_access_view(self):
+        """
+        Test checks that the view correctly returns non context when non logged in user is trying to access view
+        """
+        self.client.logout()
+        response = self.client.get(
+            reverse("complaint-add-attachment", kwargs={"pk": self.test_complaint.id}),
+            follow=True,
+        )
+
+        self.assertRedirects(response, f"/users/accounts/login/?next=/tasks/complaint/{self.test_complaint.id}")
+
+    def test_should_block_when_adding_more_than_ten_attachments(self):
+        """
+        Test checks that view blocks adding more then ten attachments.
+        """
+        for i in range(10):
+            self.attachment_new = SimpleUploadedFile(f"test_file_{i}.txt", b"content of test file")
+            self.client.post(
+                reverse("complaint-add-attachment", kwargs={"pk": self.test_complaint.id}),
+                data={
+                    "complaint": self.test_complaint.id,
+                    "attachment": self.attachment_new,
+                },
+            )
+
+        attachment_eleven = SimpleUploadedFile("test_file_11.txt", b"content of test file")
+        self.client.post(
+            reverse("complaint-add-attachment", kwargs={"pk": self.test_complaint.id}),
+            data={
+                "complaint": self.test_complaint.id,
+                "attachment": attachment_eleven,
+            },
+        )
+        self.assertEqual(len(self.test_complaint.attachments.all()), 10)
+
+    def test_should_raise_exception_when_adding_attachment_file_with_non_content(self):
+        """
+        Test checks that it is not possible to add attachment with non content.
+        """
+        empty_file = SimpleUploadedFile("test_file.txt", b"", content_type="text/plain")
+        response = self.client.post(
+            reverse("complaint-add-attachment", kwargs={"pk": self.test_complaint.id}),
+            data={
+                "complaint": self.test_complaint.id,
+                "attachment": empty_file,
+            },
+            follow=True,
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(self.test_complaint.attachments.all()), 0)
+        with self.assertRaises(ObjectDoesNotExist):
+            ComplaintAttachment.objects.get(complaint=self.test_complaint)
+
+    def test_should_block_adding_attachment_with_non_supported_content_type(self):
+        """
+        Test checks that it is not possible to add attachment with non supported content type.
+        """
+        wrong_file = SimpleUploadedFile("test_file.jpg", b"content of test file", content_type="image/jpeg")
+        response = self.client.post(
+            reverse("complaint-add-attachment", kwargs={"pk": self.test_complaint.id}),
+            data={
+                "complaint": self.test_complaint.id,
+                "attachment": wrong_file,
+            },
+            follow=True,
+        )
+
+        self.assertEqual(len(self.test_complaint.attachments.all()), 0)
+        self.assertContains(response, "File type is not supported")
+
+
+class TestComplaintAttachmentDeleteView(TestCase):
+    """
+    Test case for delete complaint attachment
+    """
+
+    def setUp(self) -> None:
+        """
+        Set up method that is run before every individual test.
+        Here it prepares test user, complaint, complaint attachment.
+        """
+        super().setUp()
+        self.user = UserFactory.create()
+        self.test_complaint = ComplaintFactory(complainant=self.user)
+        self.attachment = ComplaintAttachmentFactory(complaint=self.test_complaint)
+        self.client.login(username=self.user.username, password="secret")
+
+    def tearDown(self) -> None:
+        """
+        Clean up method after each test case. Deletes all created attachments files.
+        """
+        file_path = settings.MEDIA_ROOT / ATTACHMENTS_PATH
+        shutil.rmtree(file_path, ignore_errors=True)
+        User.objects.all().delete()
+        Group.objects.all().delete()
+        super().tearDown()
+
+    def test_should_block_unauthorized_complaint_attachment_delete_access(self):
+        """
+        Test check if it is possible for another user to delete an attachment to the complaint
+        and redirect to proper url.
+        """
+        other_user = UserFactory.create()
+        other_complaint = ComplaintFactory.create(complainant=other_user)
+        other_attachment = ComplaintAttachmentFactory.create(complaint=other_complaint)
+        response = self.client.post(reverse("complaint-attachment-delete", kwargs={"pk": other_attachment.id}))
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse("complaint-detail", kwargs={"pk": other_complaint.id}))
+        self.assertTrue(ComplaintAttachment.objects.filter(id=other_attachment.id).exists())
+
+    def test_should_delete_complaint_attachment_by_complaint_creator_and_redirect_to_proper_url(self):
+        """
+        Test case to check if complaint attachment is deleted by user-owner of complaint and redirect to proper url
+        """
+        response = self.client.post(reverse("complaint-attachment-delete", kwargs={"pk": self.attachment.id}))
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse("complaint-detail", kwargs={"pk": self.test_complaint.id}))
+        self.test_complaint.refresh_from_db()
+        self.assertFalse(ComplaintAttachment.objects.filter(id=self.attachment.id).exists())
+
+    def test_should_delete_complaint_attachment_by_moderator_and_redirects_to_proper_url(self):
+        """
+        This test case checks if complaint attachment is deleted by moderator and redirect to proper url
+        """
+        self.user_moderator = UserFactory.create()
+        moderator_group, created = Group.objects.get_or_create(name=settings.GROUP_NAMES.get("MODERATOR"))
+        self.user_moderator.groups.add(moderator_group)
+        self.client.login(username=self.user_moderator.username, password="secret")
+
+        response = self.client.post(reverse("complaint-attachment-delete", kwargs={"pk": self.attachment.id}))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse("complaint-detail", kwargs={"pk": self.test_complaint.id}))
+        self.test_complaint.refresh_from_db()
+        self.assertFalse(ComplaintAttachment.objects.filter(id=self.attachment.id).exists())
+
+    def test_should_handle_attempt_to_delete_nonexistent_complaint_attachment(self):
+        """
+        Test case to check if attempt to delete non existent complaint attachment returns 404
+        """
+        temp_attachment = ComplaintAttachmentFactory(complaint=self.test_complaint)
+        temp_attachment_id = temp_attachment.id
+        temp_attachment.delete()
+        response = self.client.post(reverse("complaint-attachment-delete", kwargs={"pk": temp_attachment_id}))
+
+        self.assertEqual(response.status_code, 404)
+        self.assertFalse(ComplaintAttachment.objects.filter(id=temp_attachment_id).exists())
 
 
 class TestTaskDownloadMethod(TestCase):
