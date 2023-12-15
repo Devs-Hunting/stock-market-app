717ddb56302499acdfa53f7bc27570395e885a23
Bartosz Wisniewski <bartekwisnia@op.pl>
2023-12-15 09:42:14 +0100

    dashboard view refactored

--- src/psmproject/dashboardapp/views.py
+++ src/psmproject/dashboardapp/views.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from chatapp.models import Message
 from django.db.models import Q
 from django.views.generic.base import TemplateView
@@ -7,37 +9,58 @@ from tasksapp.models import Offer, Task
 class DashboardView(TemplateView):
     template_name = "dashboardapp/dashboard.html"
 
+    def get_users_tasks(self):
+        return Task.objects.filter(Q(client=self.request.user) & Q(status__lte=Task.TaskStatus.OBJECTIONS))
+
+    def get_users_jobs(self):
+        return Task.objects.filter(
+            Q(selected_offer__isnull=False)
+            & Q(status__lte=Task.TaskStatus.OBJECTIONS)
+            & Q(selected_offer__contractor=self.request.user)
+        )
+
+    def get_user_offers(self):
+        return Offer.objects.filter(Q(contractor=self.request.user) & Q(accepted=False))
+
+    def get_new_offers(self):
+        offers = self.get_user_offers()
+        return offers.filter(task__selected_offer__isnull=True).order_by("-created")[:5]
+
+    def get_lost_offers(self):
+        offers = self.get_user_offers()
+        return offers.filter(task__selected_offer__isnull=False).order_by("-task__updated")[:5]
+
+    def get_new_messages(self):
+        return (
+            Message.objects.filter(chat__participants__user=self.request.user)
+            .exclude(author=self.request.user)
+            .order_by("-timestamp")[:5]
+        )
+
+    @staticmethod
+    def last_tasks_filtered_by_status(tasks, statuses: List[int]):
+        return tasks.filter(status__in=statuses).order_by("-updated")[:5]
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
         if not self.request.user.is_authenticated:
             return context
 
-        my_active_tasks = Task.objects.filter(Q(client=self.request.user) & Q(status__lte=Task.TaskStatus.OBJECTIONS))
+        tasks = self.get_users_tasks()
+        jobs = self.get_users_jobs()
 
-        my_active_jobs = Task.objects.filter(
-            Q(selected_offer__isnull=False)
-            & Q(status__lte=Task.TaskStatus.OBJECTIONS)
-            & Q(selected_offer__contractor=self.request.user)
-        )
-
-        my_offers = Offer.objects.filter(Q(contractor=self.request.user) & Q(accepted=False))
-        context["tasks"] = my_active_tasks.filter(status=Task.TaskStatus.ON_GOING).order_by("-updated")[:5]
-        context["new_tasks"] = my_active_tasks.filter(status__lte=Task.TaskStatus.ON_HOLD).order_by("-updated")[:5]
-        context["problematic_tasks"] = my_active_tasks.filter(status=Task.TaskStatus.OBJECTIONS).order_by("-updated")[
-            :5
-        ]
-
-        context["jobs"] = my_active_jobs.filter(status=Task.TaskStatus.ON_GOING).order_by("-updated")[:5]
-        context["problematic_jobs"] = my_active_jobs.filter(status=Task.TaskStatus.OBJECTIONS).order_by("-updated")[:5]
-
-        context["new_offers"] = my_offers.filter(task__selected_offer__isnull=True).order_by("-created")[:5]
-        context["lost_offers"] = my_offers.filter(task__selected_offer__isnull=False).order_by("-task__updated")[:5]
-
-        context["new_messages"] = (
-            Message.objects.filter(chat__participants__user=self.request.user)
-            .exclude(author=self.request.user)
-            .order_by("-timestamp")[:5]
+        context.update(
+            {
+                "tasks": self.last_tasks_filtered_by_status(tasks, [Task.TaskStatus.ON_GOING]),
+                "new_tasks": self.last_tasks_filtered_by_status(tasks, [Task.TaskStatus.OPEN, Task.TaskStatus.ON_HOLD]),
+                "problematic_tasks": self.last_tasks_filtered_by_status(tasks, [Task.TaskStatus.OBJECTIONS]),
+                "jobs": self.last_tasks_filtered_by_status(jobs, [Task.TaskStatus.ON_GOING]),
+                "problematic_jobs": self.last_tasks_filtered_by_status(jobs, [Task.TaskStatus.OBJECTIONS]),
+                "new_offers": self.get_new_offers(),
+                "lost_offers": self.get_lost_offers(),
+                "new_messages": self.get_new_messages(),
+            }
         )
 
         return context
