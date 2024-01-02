16644f5057e72d4cde7cefc5e5d11df5ac01a370
Bartosz Wisniewski <bartekwisnia@op.pl>
2024-01-02 09:52:13 +0100

    added postgres DB container to local docker compose and django settings

--- src/psmproject/psmproject/settings/base.py
+++ src/psmproject/psmproject/settings/base.py
@@ -101,12 +101,25 @@ WSGI_APPLICATION = "psmproject.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+
+if "DB_ENGINE" in os.environ:
+    DATABASES = {
+        "default": {
+            "ENGINE": os.environ["DB_ENGINE"],
+            "NAME": os.environ["DB_DB"],
+            "USER": os.environ["DB_USER"],
+            "PASSWORD": os.environ["DB_PASSWORD"],
+            "HOST": os.environ["DB_HOST"],
+            "PORT": os.environ["DB_PORT"],
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
