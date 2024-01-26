980dc35962a19e741a5a8b852898574d221a1b24
ndestrieux <n.destrieux@gmail.com>
2024-01-26 19:28:25 +0100

    [DEV-119] Tests reviewed, different Payment instance cases create in setup class.

--- src/psmproject/tasksapp/tests/models/test_payments.py
+++ src/psmproject/tasksapp/tests/models/test_payments.py
@@ -8,77 +8,74 @@ class TestPaymentModel(TestCase):
     Tests for Payment model
     """
 
-    def setUp(self) -> None:
+    @classmethod
+    def setUpClass(cls) -> None:
         super().setUpClass()
-        self.payment = PaymentFactory(total_cost=1000)
+        cls.new_payment = PaymentFactory(total_cost=1000)
+        cls.payment_advance_paid = PaymentFactory(total_cost=1000, amount_paid=170)
+        cls.payment_completed = PaymentFactory(total_cost=1000, amount_paid=1000)
 
     def test_should_confirm_payment_instance_has_been_created_properly(self):
         """
-        Test checks if payment has been properly created in DB.
+        Test checks if payment instance has been properly created in DB.
         """
         payments = Payment.objects.all()
-        self.assertIn(self.payment, payments)
+        self.assertIn(self.new_payment, payments)
 
     def test_should_return_correct_value_for_amount_due_to_contractor_field(self):
         """
         Test checks if amount due to contractor value is returned correctly, by default service fee is 15% of total
         amount, then amount due to contractor is 85% of the total cost.
         """
-        self.assertEqual(self.payment.amount_due_to_contractor, 850)
+        self.assertEqual(self.new_payment.amount_due_to_contractor, 850)
 
     def test_should_return_correct_value_for_advance_amount_field(self):
         """
         Test checks if advance amount value is returned correctly, by default advance is 20% of amount due to
         contractor.
         """
-        self.assertEqual(self.payment.advance_amount, 170)
+        self.assertEqual(self.new_payment.advance_amount, 170)
 
     def test_should_return_correct_service_fee_amount(self):
         """
         Test checks if service fee amount is returned correctly.
         """
-        self.assertEqual(self.payment.service_fee, 150)
+        self.assertEqual(self.new_payment.service_fee, 150)
 
     def test_should_return_correct_boolean_value_from_advance_paid_property_when_advance_not_paid_yet(self):
         """
         Test checks if advance_paid property boolean value is false when advance has not been paid yet.
         """
-        self.assertFalse(self.payment.advance_paid)
+        self.assertFalse(self.new_payment.advance_paid)
 
     def test_should_return_correct_boolean_value_from_completed_property_when_payment_not_completed_yet(self):
         """
         Test checks if completed property boolean value is false when payment has not been completed yet.
         """
-        self.assertFalse(self.payment.completed)
+        self.assertFalse(self.new_payment.completed)
 
     def test_should_return_correct_boolean_value_from_advance_paid_property_when_advance_paid(self):
         """
         Test checks if advance_paid property boolean value is true when advance has been paid.
         """
-        self.payment.amount_paid = self.payment.advance_amount
-        self.payment.save(update_fields=["amount_paid"])
-        self.assertTrue(self.payment.advance_paid)
+        self.assertTrue(self.payment_advance_paid.advance_paid)
 
     def test_should_return_correct_boolean_value_from_completed_property_payment_completed(self):
         """
         Test checks if completed property boolean value is true when payment has been completed.
         """
-        self.payment.amount_paid = self.payment.total_cost
-        self.payment.save(update_fields=["total_cost"])
-        self.assertTrue(self.payment.completed)
+        self.assertTrue(self.payment_completed.completed)
 
     def test_should_return_correct_str_representation_when_payment_not_completed(self):
         """
         Test checks if str representation is correct when payment has not been completed yet.
         """
-        expected_str = f"Payment: {self.payment.total_cost}"
-        self.assertEqual(str(self.payment), expected_str)
+        expected_str = f"Payment: {self.new_payment.total_cost}"
+        self.assertEqual(str(self.new_payment), expected_str)
 
     def test_should_return_correct_str_representation_when_payment_completed(self):
         """
         Test checks if str representation is correct when payment has been completed.
         """
-        self.payment.amount_paid = self.payment.total_cost
-        self.payment.save(update_fields=["total_cost"])
-        expected_str = f"Payment: {self.payment.total_cost} - COMPLETED"
-        self.assertEqual(str(self.payment), expected_str)
+        expected_str = f"Payment: {self.payment_completed.total_cost} - COMPLETED"
+        self.assertEqual(str(self.payment_completed), expected_str)
