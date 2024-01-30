aeb57cb909de3be360a03cc7ed7c2ef91866dc03
Bartosz Wisniewski <bartekwisnia@op.pl>
2024-01-30 20:06:33 +0100

    use test server url

--- src/psmproject/usersapp/tests/test_live.py
+++ src/psmproject/usersapp/tests/test_live.py
@@ -1,5 +1,6 @@
 from django.test import LiveServerTestCase
 from django.urls import reverse
+from factories.factories import UserFactory
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -14,13 +15,22 @@ class TestLogIn(LiveServerTestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.base_url = "http://localhost:8000"
+        cls.base_url = cls.live_server_url
+        cls.password = "pass"
+        cls.user = UserFactory(password=cls.password)
+        # options = Options()
+        # options.add_argument("--headless")
+        # options.add_argument("--window-size=1920, 1080")
+        # options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Geck"
+        #                      "o) Chrome/87.0.4280.88 Safari/537.36")
+        # cls.driver = webdriver.Chrome(options=options)
         cls.driver = webdriver.Chrome()
         cls.driver.implicitly_wait(10)
         cls.url = cls.base_url + reverse(TestLogIn.url_name)
 
     @classmethod
     def tearDownClass(cls):
+        cls.user.delete()
         cls.driver.quit()
         super().tearDownClass()
 
@@ -28,8 +38,8 @@ class TestLogIn(LiveServerTestCase):
         self.driver.get(self.url)
         user_email = self.driver.find_element(By.ID, "id_login")
         user_password = self.driver.find_element(By.ID, "id_password")
-        user_email.send_keys("nowak@nowak.pl")
-        user_password.send_keys("useruser")
+        user_email.send_keys(self.user.email)
+        user_password.send_keys(self.password)
         submit = self.driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
         submit.click()
         self.assertEqual(self.driver.current_url, self.base_url + "/users/profile/")
