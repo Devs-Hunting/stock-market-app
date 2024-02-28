6e533d4bde7a24400eddb703bd893bfddfb0f127
Nicolas Destrieux <42584510+ndestrieux@users.noreply.github.com>
2024-02-28 09:36:29 +0100

    [DEV-126] [FIX] remove warnings in tests (#77)
    * [DEV-126] All datetime made timezone aware.
    
    * [DEV-126] All datetime made timezone aware.

--- src/psmproject/usersapp/tasks.py
+++ src/psmproject/usersapp/tasks.py
@@ -1,8 +1,7 @@
-from datetime import datetime
-
 from celery import shared_task
 from django.conf import settings
 from django.contrib.auth.models import Group
+from django.utils.timezone import now
 from usersapp.models import BlockedUser
 
 BLOCKED_USER_GROUP = Group.objects.get(name=settings.GROUP_NAMES.get("BLOCKED_USER"))
@@ -11,7 +10,7 @@ BLOCKED_USER_GROUP = Group.objects.get(name=settings.GROUP_NAMES.get("BLOCKED_US
 @shared_task
 def unblock_users():
     for user in BLOCKED_USER_GROUP.user_set.all():
-        active_ban_exists = BlockedUser.objects.filter(blocked_user=user, blocking_end_date__gt=datetime.now()).exists()
+        active_ban_exists = BlockedUser.objects.filter(blocked_user=user, blocking_end_date__gt=now()).exists()
 
         if not active_ban_exists:
             BLOCKED_USER_GROUP.user_set.remove(user)
