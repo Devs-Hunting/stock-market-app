6fa05171b82dcff35233df46f6fd7bc6f0a88b1e
ndestrieux <n.destrieux@gmail.com>
2024-03-07 22:52:13 +0100

    [DEV-101] Signal for creating chat when arbiter is selecting a complaint to take care of.

--- src/psmproject/chatapp/signals.py
+++ src/psmproject/chatapp/signals.py
@@ -1,7 +1,7 @@
 from chatapp.models import Chat, Participant, RoleChoices
 from django.dispatch import receiver
 from fieldsignals import post_save_changed
-from tasksapp.models import Task
+from tasksapp.models import Complaint, Task
 
 
 @receiver(post_save_changed, sender=Task, fields=["selected_offer"])
@@ -10,3 +10,12 @@ def create_task_related_chat(sender, instance, **kwargs):
         chat = Chat.objects.create(content_object=instance)
         Participant.objects.create(user=instance.client, chat=chat, role=RoleChoices.CLIENT)
         Participant.objects.create(user=instance.selected_offer.contractor, chat=chat, role=RoleChoices.CONTRACTOR)
+
+
+@receiver(post_save_changed, sender=Complaint, fields=["arbiter"])
+def create_complaint_related_chat(sender, instance, **kwargs):
+    if instance.arbiter:
+        chat = Chat.objects.create(content_object=instance)
+        Participant.objects.create(user=instance.task.client, chat=chat, role=RoleChoices.CLIENT)
+        Participant.objects.create(user=instance.task.selected_offer.contractor, chat=chat, role=RoleChoices.CONTRACTOR)
+        Participant.objects.create(user=instance.arbiter, chat=chat, role=RoleChoices.ARBITER)
