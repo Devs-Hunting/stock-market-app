00df5309e13d3bff8d559b4c20cfd62b70bb1eee
ndestrieux <n.destrieux@gmail.com>
2024-03-08 17:15:03 +0100

    [DEV-101] Signals turned off for chat complaint creation when in test.

--- src/psmproject/chatapp/signals.py
+++ src/psmproject/chatapp/signals.py
@@ -2,6 +2,7 @@ from chatapp.models import Chat, Participant, RoleChoices
 from django.dispatch import receiver
 from fieldsignals import post_save_changed
 from tasksapp.models import Complaint, Task
+from tasksapp.utils import receiver_not_in_test
 
 
 @receiver(post_save_changed, sender=Task, fields=["selected_offer"])
@@ -12,7 +13,7 @@ def create_task_related_chat(sender, instance, **kwargs):
         Participant.objects.create(user=instance.selected_offer.contractor, chat=chat, role=RoleChoices.CONTRACTOR)
 
 
-@receiver(post_save_changed, sender=Complaint, fields=["arbiter"])
+@receiver_not_in_test(post_save_changed, sender=Complaint, fields=["arbiter"])
 def create_complaint_related_chat(sender, instance, **kwargs):
     if instance.arbiter:
         chat = Chat.objects.create(content_object=instance)
