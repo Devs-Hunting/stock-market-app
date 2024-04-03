37fe927c237a7e7f2f393df37ffdea9729d05458
bartwisniewski <bartekwisnia@op.pl>
2024-04-03 11:44:31 +0200

    switched to console email backend for mikrus, added initadmin script

--- src/psmproject/psmproject/settings/mikrus.py
+++ src/psmproject/psmproject/settings/mikrus.py
@@ -24,9 +24,14 @@ CELERY_RESULT_BACKEND = os.environ["CELERY_RESULT_BACKEND"]
 
 MEDIA_URL = os.environ["HOST_NAME"] + "/media/"
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = os.environ["EMAIL_HOST"]
-EMAIL_PORT = 465
-EMAIL_USE_SSL = True
-EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
-EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# EMAIL_HOST = os.environ["EMAIL_HOST"]
+# EMAIL_PORT = 465
+# EMAIL_USE_SSL = True
+# EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
+# EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
+ADMIN_USER = os.environ.get("ADMIN_USER", "admin")
+ADMIN_EMAIL = os.environ.get("ADMIN_EMAIL", "admin@admin.com")
+ADMIN_PASS = os.environ.get("ADMIN_PASS", "pass")
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_SENDER = "from@example.com"
