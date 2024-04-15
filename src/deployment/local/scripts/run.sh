940d4ac39932365b1af3167dddaa3f0027c9b9da
bartwisniewski <bartekwisnia@op.pl>
2024-04-15 11:02:09 +0200

    settings, language switching modal+ test polish translation

--- src/deployment/local/scripts/run.sh
+++ src/deployment/local/scripts/run.sh
@@ -5,5 +5,5 @@ set -e
 python manage.py makemigrations
 python manage.py migrate
 python manage.py load_fixtures
-# python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 python manage.py runserver 0.0.0.0:8000
