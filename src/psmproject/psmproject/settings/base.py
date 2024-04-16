240e03ec5eea98f6cc03dffc72f97bc2738b1c59
bartwisniewski <bartekwisnia@op.pl>
2024-04-16 19:14:07 +0200

    moved project-global static files to project_static folder and added this to staticfile_dirs in settings

--- src/psmproject/psmproject/settings/base.py
+++ src/psmproject/psmproject/settings/base.py
@@ -170,10 +170,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "static"
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-# ]
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "project_static")]
 # Media files
 MEDIA_FILES = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
