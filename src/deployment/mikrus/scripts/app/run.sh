5dbbb6545474dfe29f9208b988d2f20d10bb186d
ndestrieux <n.destrieux@gmail.com>
2024-04-19 12:27:42 +0200

    [DEV-135] starting asgi and wsgi in same container.

--- src/deployment/mikrus/scripts/app/run.sh
+++ src/deployment/mikrus/scripts/app/run.sh
@@ -6,4 +6,4 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py initadmin
 python manage.py collectstatic --noinput
-gunicorn -b 0.0.0.0:8000 psmproject.wsgi:application
+daphne -b 0.0.0.0 -p 8001 psmproject.asgi:application & gunicorn -b 0.0.0.0:8000 psmproject.wsgi:application
--- src/deployment/mikrus/scripts/websocket/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-set -e
-
-daphne -b 0.0.0.0 -p 8001 psmproject.asgi:application
