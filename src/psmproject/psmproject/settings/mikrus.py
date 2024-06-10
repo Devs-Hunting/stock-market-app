afdcc6864248e1c86dd89449d8026c0ba9d56cb5
ndestrieux <n.destrieux@gmail.com>
2024-06-10 18:53:29 +0200

    [DEV-148] Duplicate variables removed and getting env variable with .get().

--- src/psmproject/psmproject/settings/mikrus.py
+++ src/psmproject/psmproject/settings/mikrus.py
@@ -2,20 +2,7 @@ import os
 
 from .base import *  # noqa
 
-SECRET_KEY = os.environ["SECRET_KEY"]
 DEBUG = False
-ALLOWED_HOSTS = ["*"]
-
-DATABASES = {
-    "default": {
-        "ENGINE": os.environ["DB_ENGINE"],
-        "NAME": os.environ["POSTGRES_DB"],
-        "USER": os.environ["POSTGRES_USER"],
-        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
-        "HOST": os.environ["POSTGRES_HOST"],
-        "PORT": os.environ["POSTGRES_PORT"],
-    }
-}
 
 TEST_HOST = f"http://localhost:{os.environ['IP4_PORT']}"
 CSRF_TRUSTED_ORIGINS = [os.environ["HOST_NAME"], TEST_HOST]
@@ -26,8 +13,8 @@ CELERY_RESULT_BACKEND = REDIS_URL
 MEDIA_URL = os.environ["HOST_NAME"] + "/media/"
 
 EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
-DEFAULT_FROM_EMAIL = os.environ["DEFAULT_FROM_EMAIL"]
-SENDGRID_API_KEY = os.environ["SENDGRID_API_KEY"]
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
+SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY")
 
 ADMIN_USER = os.environ.get("ADMIN_USER")
 ADMIN_EMAIL = os.environ.get("ADMIN_EMAIL")
